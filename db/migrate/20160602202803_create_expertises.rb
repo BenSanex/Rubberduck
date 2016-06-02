55e4b6c5b472b46f00c159dd1cb442d087fee923
dennyjovic <dennyjovic@gmail.com>
2016-06-02 15:45:36 -0500

    filled in migration files and changed inheritance for mentors and students

--- db/migrate/20160602202803_create_expertises.rb
+++ db/migrate/20160602202803_create_expertises.rb
@@ -1,6 +1,9 @@
 class CreateExpertises < ActiveRecord::Migration[5.0]
   def change
     create_table :expertises do |t|
+    	t.integer :mentor_id
+    	t.integer :skills_id
+    	t.integer :level
 
       t.timestamps
     end
