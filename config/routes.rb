781501e251cd0bbcc57479fb5420e0bda60b8b6c
Scott Southard <scottmsouthard@gmail.com>
2016-06-02 16:56:27 -0500

    Route Start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  resources :mentors, only: [:new, :create, :show]
+  resources :students, only: [:new, :create, :show]
+
+  root 'welcome#index'
+
 end
