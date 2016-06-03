4bde202fe03ef9f175e13ff6246c3aa752087caf
Scott Southard <scottmsouthard@gmail.com>
2016-06-03 15:44:25 -0500

    Redo associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
-	belongs_to :mentor, class_name: "User"
-	belongs_to :student, class_name: "User"
+	belongs_to :mentor, class_name: "User", optional: true
+	belongs_to :student, class_name: "User", optional: true
 	has_one :rating
-	belongs_to :skill
+	belongs_to :skill, optional: true
 	
 end
