c415237768124f28cf8768eae3c2f5047eacda3b
dennyjovic <dennyjovic@gmail.com>
2016-06-03 11:02:47 -0500

    added hover anim for the squares

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
 
 	// Hover swap for login buttons
 	$(".student-square").on("mouseenter", function(){
-		$(".student-name").stop(true, true).fadeOut(100);
-		$(".student-hover").stop(true, true).fadeIn("slow");
+		$(".student-name").stop(true, true).hide();
+		$(".student-hover").stop(true, true).fadeIn(100);
 	});
 
 	$(".student-square").on("mouseleave", function(){
 		$(".student-hover").stop(true, true).hide();
-		$(".student-name").stop(true, true).fadeIn("slow");
+		$(".student-name").stop(true, true).fadeIn(100);
 	});
 
-		$(".mentor-square").on("mouseenter", function(){
-		$(".mentor-name").stop(true, true).fadeOut(100);
-		$(".mentor-hover").stop(true, true).fadeIn("slow");
+	$(".mentor-square").on("mouseenter", function(){
+		$(".mentor-name").stop(true, true).hide();
+		$(".mentor-hover").stop(true, true).fadeIn(100);
 	});
 
 	$(".mentor-square").on("mouseleave", function(){
 		$(".mentor-hover").stop(true, true).hide();
-		$(".mentor-name").stop(true, true).fadeIn("slow");
+		$(".mentor-name").stop(true, true).fadeIn(100);
 	});
 
 });
