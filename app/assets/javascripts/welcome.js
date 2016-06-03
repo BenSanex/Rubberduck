06666790b94e666aab6aea54f83ce1be94806239
dennyjovic <dennyjovic@gmail.com>
2016-06-03 10:58:34 -0500

    fade in for login buttons

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
 
 	// Hover swap for login buttons
 	$(".student-square").on("mouseenter", function(){
-		$(".student-hover").show();
-		$(".student-name").hide();
+		$(".student-name").stop(true, true).fadeOut(100);
+		$(".student-hover").stop(true, true).fadeIn("slow");
 	});
 
 	$(".student-square").on("mouseleave", function(){
-		$(".student-hover").hide();
-		$(".student-name").show();
+		$(".student-hover").stop(true, true).hide();
+		$(".student-name").stop(true, true).fadeIn("slow");
 	});
 
-	$(".mentor-square").on("mouseenter", function(){
-		$(".mentor-hover").show();
-		$(".mentor-name").hide();
+		$(".mentor-square").on("mouseenter", function(){
+		$(".mentor-name").stop(true, true).fadeOut(100);
+		$(".mentor-hover").stop(true, true).fadeIn("slow");
 	});
 
 	$(".mentor-square").on("mouseleave", function(){
-		$(".mentor-hover").hide();
-		$(".mentor-name").show();
+		$(".mentor-hover").stop(true, true).hide();
+		$(".mentor-name").stop(true, true).fadeIn("slow");
 	});
 
 });
