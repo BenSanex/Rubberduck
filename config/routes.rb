68b62cc389f4835aed0492221964ca56ee0d5455
BenSanex <ben.sanecki@gmail.com>
2016-06-03 13:26:47 -0500

    noob

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
 
   resources :students, only: [:new, :create, :show]
   resources :mentors, only: [:new, :create, :show]
-
+  resources :questions
   root 'welcome#index'
 
 end
