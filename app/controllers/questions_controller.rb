6dec16a65baad591fc27cb6dc9e4e7e8e641987d
BenSanex <ben.sanecki@gmail.com>
2016-06-03 16:20:05 -0500

    broadcast and listening

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,25 @@
 class QuestionsController < ApplicationController
- 
+  def index
+  end
+
   def show
   	@user = User.find(current_user.id)
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      ActionCable.server.broadcast 'questions',
+        content: @question.content,
+        title: @question.title
+      head :ok
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content, :title)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :questions
   root 'welcome#index'
 
+  mount ActionCable.server => '/cable'
 end
