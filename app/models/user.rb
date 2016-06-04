879e65b5d67ceb5ad356dcf8768294e5c0626149
Scott Southard <scottmsouthard@gmail.com>
2016-06-04 11:29:43 -0500

    Attribute accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-	 devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+	attr_accessor :email
+	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 	has_many :expertises, foreign_key: :mentor_id
 	has_many :skills, through: :expertises 
 	has_many :asked_questions, class_name: "Question", foreign_key: :student_id
