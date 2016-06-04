9ede4418a5a685a426484116c23d1b3dd433c967
Scott Southard <scottmsouthard@gmail.com>
2016-06-04 12:14:56 -0500

    local requests method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-	attr_accessor :email, :full_name, :username, :avatar, :is_mentor, :encrypted_password
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+	attr_accessor :email, :full_name, :username, :avatar, :is_mentor, :encrypted_password
 	has_many :expertises, foreign_key: :mentor_id
 	has_many :skills, through: :expertises 
 	has_many :asked_questions, class_name: "Question", foreign_key: :student_id
 	has_many :proposed_questions, class_name: "Question", foreign_key: :mentor_id
+
+	def email_changed?
+  		false
+	end
 end
