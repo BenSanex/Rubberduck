d50042773e094adfe92107acfcb8670e06e2306b
Scott Southard <scottmsouthard@gmail.com>
2016-06-05 16:48:37 -0500

    bug fixing

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -9,13 +9,9 @@ class MentorsController < ApplicationController
   	questions = Question.where(mentor_id: @user.id)
   	if questions != []
       all_ratings = questions.map{ |question| question.rating }
-      if all_ratings != []
-      	rating_total = all_ratings.map {|rating| rating.number.to_f }
-      	rating  = rating_total.inject(0){|sum,x| sum + x } / rating_total.length
-      	@rating = rating.round(1)
-      else
-        @rating = 0
-      end
+    	rating_total = all_ratings.map {|rating| rating.number.to_f }
+    	rating  = rating_total.inject(0){|sum,x| sum + x } / rating_total.length
+    	@rating = rating.round(1)
     end
   	## Find Skill
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   	has_secure_password
 	has_many :expertises, foreign_key: :mentor_id
-	has_many :skills, through: :expertises 
+	has_many :skills, through: :expertises
 	has_many :asked_questions, class_name: "Question", foreign_key: :student_id
 	has_many :proposed_questions, class_name: "Question", foreign_key: :mentor_id
 
