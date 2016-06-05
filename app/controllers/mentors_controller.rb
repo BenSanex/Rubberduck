bad3ec4c6f1616898eea8ce56bbf965312f5d990
Scott Southard <scottmsouthard@gmail.com>
2016-06-05 17:33:54 -0500

    assets organized

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -5,15 +5,6 @@ class MentorsController < ApplicationController
 
   def show
   	@user = User.find(current_user.id)
-  	## Find Rating
-  	# questions = Question.where(mentor_id: @user.id)
-  	# if questions != []
-    #   all_ratings = questions.map{ |question| question.rating }
-    # 	rating_total = all_ratings.map {|rating| rating.number.to_f }
-    # 	rating  = rating_total.inject(0){|sum,x| sum + x } / rating_total.length
-    # 	@rating = rating.round(1)
-    # end
-  	## Find Skill
   end
 
 end
