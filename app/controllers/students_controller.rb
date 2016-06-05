5c2060067db0db90bf264b8559ddf5c77dbceaff
Scott Southard <scottmsouthard@gmail.com>
2016-06-05 18:16:02 -0500

    no sounds?

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,6 @@ class StudentsController < ApplicationController
     @skills = Skill.all
     @question = Question.new
   	@user = User.find(current_user.id)
-
     @questions = @user.asked_questions.where('created_at >= ?', 14.days.ago)
   end
 
