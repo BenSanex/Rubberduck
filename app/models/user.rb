b77477d9c5e23b10bcd3a66f361ab4556ba2e999
Scott Southard <scottmsouthard@gmail.com>
2016-06-05 16:56:38 -0500

    bug fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,17 @@ class User < ApplicationRecord
 	def rating
 		questions = Question.where(mentor_id: self.id)
 	  	all_ratings = questions.map{ |question| question.rating }
-	  	rating_total = all_ratings.map do |rating|
-        if rating != nil
-          rating.number.to_f
-        end 
-	  	if rating_total.length > 0
-	  		rating  = rating_total.inject(0){|sum,x| sum + x } / rating_total.length
-	  		return rating.round(1)
-	  	else
-	  		return 0
-	  	end
+      if all_ratings[0] != nil
+  	  	rating_total = all_ratings.map {|rating| rating.number.to_f }
+  	  	if rating_total.length > 0
+  	  		rating  = rating_total.inject(0){|sum,x| sum + x } / rating_total.length
+  	  		return rating.round(1)
+  	  	else
+  	  		return 0
+  	  	end
+      else
+        return 0
+      end
 	end
 
 
