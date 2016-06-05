6531b7edd689e974dc26d86ceacf5b92b7b9a51f
Scott Southard <scottmsouthard@gmail.com>
2016-06-05 15:54:19 -0500

    Bug fixing

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -9,9 +9,13 @@ class MentorsController < ApplicationController
   	questions = Question.where(mentor_id: @user.id)
   	if questions != []
       all_ratings = questions.map{ |question| question.rating }
-    	rating_total = all_ratings.map {|rating| rating.number.to_f }
-    	rating  = rating_total.inject(0){|sum,x| sum + x } / rating_total.length
-    	@rating = rating.round(1)
+      if all_ratings !=[]
+      	rating_total = all_ratings.map {|rating| rating.number.to_f }
+      	rating  = rating_total.inject(0){|sum,x| sum + x } / rating_total.length
+      	@rating = rating.round(1)
+      else
+        @rating = 0
+      end
     end
   	## Find Skill
   end
