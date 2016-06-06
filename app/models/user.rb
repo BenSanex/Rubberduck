1f1c052be568e7945082cabfaa3b9c48e5b0b9d9
Ben Sanecki <ben.sanecki@gmail.com>
2016-06-06 13:30:06 -0500

    badge method and start partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   	has_secure_password
 	has_many :expertises, foreign_key: :mentor_id
-	has_many :skills, through: :expertises
+	has_many :skills, through: :expertises, source: :skill
 	has_many :asked_questions, class_name: "Question", foreign_key: :student_id
 	has_many :answered_questions, class_name: "Question", foreign_key: :mentor_id
 	has_many :mentor_ratings, through: :answered_questions, source: :rating
@@ -20,4 +20,16 @@ class User < ApplicationRecord
     questions.length / 10
   end
 
+  def badges
+    badge_array = []
+    badge_array << '1' if self.level > 1
+    badge_array << '2' if self.level > 2
+    badge_array << '3' if self.level > 3
+    badge_array << 'ruby' if self.skills_array.include?('Ruby')
+    badge_array << 'C#' if self.skills_array.include?('C#')
+  end
+
+  def skills_array
+    self.skills.map { |skill| skill.sub_type }
+  end
 end
