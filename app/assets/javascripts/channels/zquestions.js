149d0d7c8b23de0d9515ce7fcd56ce57830e5da2
dennyjovic <dennyjovic@gmail.com>
2016-06-07 09:02:16 -0500

    fixed student page

--- app/assets/javascripts/channels/zquestions.js
+++ app/assets/javascripts/channels/zquestions.js
@@ -8,39 +8,3 @@ App.questions = App.cable.subscriptions.create('MessagesChannel', {
     return "<div id='myModal' class='modal show fade' tabindex='-1' role='dialog'><div id='moving-modal' class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h3 class='modal-title'>You have a Question!</h3></div><div class='modal-body'><h4>Question:"+data.title+"</h4><p>"+data.content+"</p></div><div class='modal-footer'><a class='btn btn-primary' href='/questions/"+data.url+"'>Enter the classroom!</a></div></div></div></div>"
   }
 });
-
-
-
-// <div id='myModal' class='modal hide fade' tabindex='-1' role='dialog'>
-//   <div class='modal-header'>
-//     <button type='button' class='close' data-dismiss='modal'>×</button>
-//       <h3 class='modal-title'>You have a Question!</h3>
-//   </div>
-//   <div class='modal-body'>
-//     <h4>"+data.title+":</h4>
-//     <p>"+data.content+"</p>
-//   </div>
-//   <div class='modal-footer'>
-//     <a class='btn btn-default pull-right' href='https://rubberduckapp.herokuapp.com/questions/"+data.url+"'>Enter the classroom!</a>
-//   </div>
-// </div>
-
-
-// received: function(data) {
-//     return $('body').append(this.renderMessage(data));
-//     $('.modal-body').empty();
-//     $('#myModal').modal({show:true});
-//     // $('#lightbox').show();
-//     $().playSound("http://www.noiseaddicts.com/samples_1w72b820/3724");
-//     // $(document).on('click','#lightbox', function() {
-//     //   $('#lightbox').hide();
-//     // });
-
-
-// // $('.thumbnail').click(function(){
-//     $('.modal-body').empty();
-//     // var title = $(this).attr("title");
-//     // $('.modal-title').html(title);
-//     // $(this.innerHTML).appendTo('.modal-body');
-//     $('#myModal').modal({show:true});
-// // });
