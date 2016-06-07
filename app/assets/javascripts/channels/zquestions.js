28f693f2f1d06e6031b6aab9e7e09fcfb3080e5b
dennyjovic <dennyjovic@gmail.com>
2016-06-07 07:29:15 -0500

    first attempt at styling the lightbox

--- app/assets/javascripts/channels/zquestions.js
+++ app/assets/javascripts/channels/zquestions.js
@@ -1,13 +1,45 @@
 App.questions = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     return $('body').append(this.renderMessage(data));
-    $('#lightbox').show();
+    $('.modal-body').empty();
+    $('#myModal').modal({show:true});
     $().playSound("http://www.noiseaddicts.com/samples_1w72b820/3724");
-    $(document).on('click','#lightbox', function() {
-      $('#lightbox').hide();
-    });
   },
   renderMessage: function(data) {
-      return "<div id='outside-lightbox'><div id='lightbox'><p>Click to close</p><div id='content'><h2>You have a Question!</h2><h3>"+data.title+":</h3><h4>"+data.content+"</h4><img src='/assets/logo'><br><a href='https://rubberduckapp.herokuapp.com/questions/"+data.url+"'>Enter the classroom!</a></div></div></div>"
+    return "<div id='myModal' class='modal hide fade' tabindex='-1' role='dialog'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'>×</button><h3 class='modal-title'>You have a Question!</h3></div><div class='modal-body'><h4>"+data.title+":</h4><p>"+data.content+"</p></div><div class='modal-footer'><a class='btn btn-default pull-right' href='https://rubberduckapp.herokuapp.com/questions/"+data.url+"'>Enter the classroom!</a></div></div>"
   }
 });
+
+// <div id='myModal' class='modal hide fade' tabindex='-1' role='dialog'>
+//   <div class='modal-header'>
+//     <button type='button' class='close' data-dismiss='modal'>×</button>
+//       <h3 class='modal-title'>You have a Question!</h3>
+//   </div>
+//   <div class='modal-body'>
+//     <h4>"+data.title+":</h4>
+//     <p>"+data.content+"</p>
+//   </div>
+//   <div class='modal-footer'>
+//     <a class='btn btn-default pull-right' href='https://rubberduckapp.herokuapp.com/questions/"+data.url+"'>Enter the classroom!</a>
+//   </div>
+// </div>
+
+
+// received: function(data) {
+//     return $('body').append(this.renderMessage(data));
+//     $('.modal-body').empty();
+//     $('#myModal').modal({show:true});
+//     // $('#lightbox').show();
+//     $().playSound("http://www.noiseaddicts.com/samples_1w72b820/3724");
+//     // $(document).on('click','#lightbox', function() {
+//     //   $('#lightbox').hide();
+//     // });
+
+
+// // $('.thumbnail').click(function(){
+//     $('.modal-body').empty();
+//     // var title = $(this).attr("title");
+//     // $('.modal-title').html(title);
+//     // $(this.innerHTML).appendTo('.modal-body');
+//     $('#myModal').modal({show:true});
+// // });
