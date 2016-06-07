161eb1e0a189143f29ceed0efa11ae56412bfddb
Ben Sanecki <ben.sanecki@gmail.com>
2016-06-07 18:20:50 -0500

    listen button for mentor

--- app/assets/javascripts/channels/zquestions.js
+++ app/assets/javascripts/channels/zquestions.js
@@ -1,10 +1,20 @@
-App.questions = App.cable.subscriptions.create('MessagesChannel', {
-  received: function(data) {
-    return $('body').append(this.renderMessage(data));
-    // $('#myModal').modal('show');
-    $().playSound("http://www.noiseaddicts.com/samples_1w72b820/3724");
-  },
-  renderMessage: function(data) {
-    return "<div id='myModal' class='modal show fade' tabindex='-1' role='dialog'><div id='moving-modal' class='modal-dialog'><div class='modal-content'><div class='modal-header'><h3 class='modal-title'>You have a Question!</h3></div><div class='modal-body'><h4>Question:"+data.title+"</h4><p>"+data.content+"</p></div><div class='modal-footer'><a class='btn btn-info' href='/mentor/profile'>Cancel</a><a class='btn btn-primary' href='/questions/"+data.url+"'>Enter the classroom!</a></div></div></div></div>"
-  }
+$(document).ready(function(){
+  var counter = 0
+  $(document).on('click','#listen',function(){
+    counter += 1
+    event.preventDefault();
+    App.questions = App.cable.subscriptions.create('MessagesChannel', {
+      received: function(data) {
+        return $('body').append(this.renderMessage(data));
+        // $('#myModal').modal('show');
+        $().playSound("http://www.noiseaddicts.com/samples_1w72b820/3724");
+      },
+      renderMessage: function(data) {
+        return "<div id='myModal' class='modal show fade' tabindex='-1' role='dialog'><div id='moving-modal' class='modal-dialog'><div class='modal-content'><div class='modal-header'><h3 class='modal-title'>You have a Question!</h3></div><div class='modal-body'><h4>Question:"+data.title+"</h4><p>"+data.content+"</p></div><div class='modal-footer'><a class='btn btn-info' href='/mentor/profile'>Cancel</a><a class='btn btn-primary' href='/questions/"+data.url+"'>Enter the classroom!</a></div></div></div></div>"
+      }
+    })
+    if (counter >= 2) {
+      location.reload();
+    }
+})
 });
