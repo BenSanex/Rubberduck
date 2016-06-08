b1787fd0205d9944dccca22fde55e23450a53ff2
Scott Southard <scottmsouthard@gmail.com>
2016-06-08 13:51:11 -0500

    portrait change

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -3,12 +3,19 @@ require 'test_helper'
 class QuestionsControllerTest < ActionController::TestCase
   setup do
     current_user = User.create(id: 1, username: 'karl', full_name: 'karl welzein', email: 'karl@gmail.com', avatar: "http://conorlastowka.com/wp-content/uploads/2011/12/karl.jpg")
-    @question = Question.create(id: 1, student_id: 1)
+    @question = questions(:one)
   end
 
-  test "should show question" do
-     get :show, id: @question
-     assert_response :success
-   end
+  # test "should show question" do
+  #    get :show, id: 1
+  #    assert_response :success
+  #  end
+
+  test "should create question" do
+      assert_difference('Question.count') do
+        post :create, question: { student_id: 1, mentor_id: 1, skill_id: 1, title: "Question:", content: "Bears, Beets, Battlestar", tag: "dwight" }
+      end
+      assert_redirected_to question_path(assigns(:question))
+    end
 
 end
