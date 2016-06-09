6c075ee216b90d8661f4a0c34cad3a6044a18b09
Scott Southard <scottmsouthard@gmail.com>
2016-06-09 11:04:11 -0500

    clean up zquestions

--- app/assets/javascripts/channels/zquestions.js
+++ app/assets/javascripts/channels/zquestions.js
@@ -8,8 +8,6 @@ $(document).ready(function(){
     App.questions = App.cable.subscriptions.create('MessagesChannel', {
       received: function(data) {
         return $('body').append(this.renderMessage(data));
-        // $('#myModal').modal('show');
-        $().playSound("http://www.noiseaddicts.com/samples_1w72b820/3724");
       },
       renderMessage: function(data) {
         return "<div id='myOtherModal' class='modal show fade' tabindex='-1' role='dialog'><div id='moving-modal' class='modal-dialog'><div class='modal-content'><div class='modal-header'><h3 class='modal-title'>You have a Question!</h3></div><div class='modal-body'><h4>Question:"+data.title+"</h4><p>"+data.content+"</p></div><div class='modal-footer'><a class='btn btn-info' href='/mentor/profile'>Cancel</a><a class='btn btn-primary' href='/questions/"+data.url+"'>Enter the classroom!</a></div></div></div></div>"
