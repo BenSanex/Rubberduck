db661bcb3b13c64d276dba2beba5997c7b769bf0
dennyjovic <dennyjovic@gmail.com>
2016-06-09 06:57:44 -0500

    added focus scroll on ask question button

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -2,5 +2,8 @@ $(function() {
   $('#question-link').on('click', function(event){
     event.preventDefault();
     $('#question-form').css('visibility', 'visible');
+    $('html, body').animate({
+	    scrollTop: $("#focus-here").offset().top
+		}, 500);
   });
 });
